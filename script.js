d04f1b9036205afc2ef7accc40748222905b5ba3
jbactasa <107353049+jbactasa@users.noreply.github.com>
2022-06-29 09:37:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//created a function to make a button
+function visitPage(){
+    window.location='https://www.youtube.com/watch?v=7KBT1goKUe4';
+}
+function visitHome(){
+    window.location='https://www.youtube.com/watch?v=7KBT1goKUe4';
+}
+function visitLogout(){
+    window.location='https://www.youtube.com/watch?v=7KBT1goKUe4';
+}
+function visitProfile(){
+    window.location='./index.html';
+}
